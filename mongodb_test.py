d12de5458493502b1885fb435391faa09dae947e
test_git <https://github.com/cssumit1/test-gitpush.git>
2022-07-17 17:56:22 +0400

    this is my first commit.

---
+++ mongodb_test.py
@@ -0,0 +1,12 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://admin:admin@ineuron.raxmhn5.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
